55083bc46c6d3db63353f5bb1bb9f0624fe2888d
HarrisonDNET <65977261+HarrisonDNET@users.noreply.github.com>
2020-05-26 14:48:22 -0500

    Install File #1
    Execute this file first in the install process

---
+++ scripttutorialinstall.sh
@@ -0,0 +1,12 @@
+clear
+echo "Greetings traveller!"
+echo "This is the installer for the script tutorial"
+echo "This will install nano (an easy text editor), and will make a new file on linux for you to get started!"
+echo "Press enter to start the install"
+read enter
+clear
+echo "To download gettingstarted.sh, press enter. This is the script for getting started."
+echo "After it's done, press enter again to install nano."
+read enter1
+clear
+wget 
