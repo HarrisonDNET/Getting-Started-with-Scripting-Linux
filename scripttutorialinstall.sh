db45aa13759af703de010b8806c780bf4e1e8395
HarrisonDNET <65977261+HarrisonDNET@users.noreply.github.com>
2020-05-27 15:21:01 -0500

    Replaced file with final

---
+++ scripttutorialinstall.sh
@@ -0,0 +1,12 @@
+clear
+echo "Greetings traveller!"
+echo "This is the installer for the script tutorial"
+echo "This will make some new files for you on your linux computer to get started!"
+echo "Visit https://github.com/HarrisonDNET/Getting-Started-with-Scripting-Linux for instructions!"
+echo "Press enter to start the install"
+read enter
+clear
+wget https://github.com/HarrisonDNET/Getting-Started-with-Scripting-Linux/archive/installationfiles.zip
+tar zxvf installationfiles.zip
+echo "Press enter when the install is done"
+read enter1
