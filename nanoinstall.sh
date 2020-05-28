6fe00f7fcbb331349dd37167331855ea463ed9e1
HarrisonDNET <65977261+HarrisonDNET@users.noreply.github.com>
2020-05-28 14:59:23 -0500

    nanoinstall.sh uploaded
    This is a script to install nano

---
+++ nanoinstall.sh
@@ -0,0 +1,8 @@
+clear
+echo "Welcome to the nano installation that I have made simpler"
+echo "This is usually used for the scripting course, but you can use this at any time"
+echo "Press enter to install nano"
+read enter
+sudo apt-get install nano
+echo "Press enter when the install is done"
+read enter1
